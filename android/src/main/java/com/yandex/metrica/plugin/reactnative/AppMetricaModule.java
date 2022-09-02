ac7aa8b217cee9c27123399562c94165cad9b0d4
Viacheslav Surzhikov <syva80@yandex.ru>
2022-09-02 23:31:08 +0300

    fix reportError

--- android/src/main/java/com/yandex/metrica/plugin/reactnative/AppMetricaModule.java
+++ android/src/main/java/com/yandex/metrica/plugin/reactnative/AppMetricaModule.java
@@ -71,12 +71,12 @@ public class AppMetricaModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void reportError(String id, String message) {
+    public void reportError(String message) {
         try {
-            Integer.valueOf("00xffWr0ng");
+          Integer.valueOf("00xffWr0ng");
         } catch (Throwable error) {
-            YandexMetrica.reportError(id, message, error);
-        }
+          YandexMetrica.reportError(message, error);
+        }   
     }
 
     @ReactMethod
